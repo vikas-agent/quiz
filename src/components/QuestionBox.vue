8f564f3e8b0dc3715c5013671a9e561d38a3e60f
vikas <vikas.cta89@gmail.com>
2019-09-25 01:07:58 +0530

    created header and questinbox basic vue

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Question Box</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'QuestionBox'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
