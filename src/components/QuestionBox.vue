cf00bc53373f621413c6c9ac58b6b2be2893732f
vikas <vikas.cta89@gmail.com>
2019-09-26 18:35:31 +0530

    shuffling the answers done

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <b-jumbotron>
-      <template >
-        <h3>{{currentQuestion.question}}</h3>  
+      <template>
+        <h3>{{currentQuestion.question}}</h3>
       </template>
 
       <hr class="my-4" />
-
-      <p v-for="(answer, index) in answers" :key="index">
-        {{answer}}
-      </p>
+      <b-list-group>
+        <b-list-group-item
+        v-for="(answer, index) in answers"
+        :key="index"
+        @click="selectAnswer(index)"
+        :class="[selectedAnswer === index ? 'selected' : '']">
+          {{answer}}
+        </b-list-group-item>
+       
+      </b-list-group>
 
       <b-button variant="success" href="#">Submit</b-button>
       <b-button variant="primary" @click="next">Next</b-button>
@@ -18,11 +24,29 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: "QuestionBox",
+  data() {
+    return {
+      selectedAnswer: null,
+      shuffledAnswers: []
+    }
+  },
   props: {
     currentQuestion: Object,
     next: Function
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedAnswer = index;
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+
+      this.shuffledAnswers = _.shuffle(answers);
+    }
   },
   computed: {
     answers() {
@@ -30,9 +54,40 @@ export default {
       answers.push(this.currentQuestion.correct_answer);
       return answers;
     }
+  },
+  watch: {
+    currentQuestion() {
+      this.selectedAnswer = null;
+      this.shuffleAnswers();
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.list-group {
+  padding-bottom : 1.5rem;
+  
+  .list-group-item:hover {
+    background-color: gray;
+    cursor: pointer;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+}
+.btn {
+  margin: 0 5px;
+}
+
+
 </style>
